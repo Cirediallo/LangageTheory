f7978f3baa004b5d116f1e9891436ee4d43f431b
Cirediallo <theevilmonster93@gmail.com>
2020-04-10 19:59:48 +0200

    test don't work

--- tp1/src/tp1/Automate.java
+++ tp1/src/tp1/Automate.java
@@ -20,29 +20,47 @@ public class Automate {
 	// Is the word reconizable by the automaton
 	public boolean isWordReconizable(String w) {
 		initState();
-		for(int i = 0; i < w.length(); i++) {
-			char c = w.charAt(i);
-			System.out.println("State -> "+state+" Entrée -> "+c);
-			nextState(c);
-		}
-		System.out.println("-> State "+state);
+		
+		for (int i = 0; i < w.length(); i++) {
+            char c = w.charAt(i);
+            System.out.println("State : "+ state + " Character: " + c);
+            nextState(c, state);
+            System.out.println("After nextstate() "+state);
+        }
+		System.out.println("-> State at the end: "+state);
 		return isCurrentStateAcceptable();
 	}
 	
-	public void nextState(char c) {
-		switch(c) {
+	//move to the next State
+	public void nextState(char c, int state) {
+		switch(state) {
 			case 0:
 				if(c == 'a')
 					state = 1;
-				else state = -1;
+				else
+					state = -1;
 				break;
 			case 1:
-				if(c == 'b')
-					state = 2;
+				if(c == 'b') state = 2;
 				else state = -1;
 				break;
 			default:
 				state = -1;
+				break;
+		}
+		
+	}
+	
+	public static void main(String args[]) {
+		Automate A =  new Automate();
+		for (int i = 0; i < args.length; i++) {
+			String arg = args[i];
+			boolean isReconizable = A.isWordReconizable(arg);
+			if(isReconizable) {
+				System.out.println("The word "+arg+" is reconizable");
+			}else {
+				System.out.println("The word "+arg+" is not reconizable");
+			}
 		}
 	}
 }
