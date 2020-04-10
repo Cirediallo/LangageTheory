b442fe47805e604ab3d2cc61c4e48c8042cbb6de
Cirediallo <theevilmonster93@gmail.com>
2020-04-10 20:14:26 +0200

    test work as expected

--- tp1/src/tp1/Automate.java
+++ tp1/src/tp1/Automate.java
@@ -20,47 +20,41 @@ public class Automate {
 	// Is the word reconizable by the automaton
 	public boolean isWordReconizable(String w) {
 		initState();
-		
 		for (int i = 0; i < w.length(); i++) {
-            char c = w.charAt(i);
-            System.out.println("State : "+ state + " Character: " + c);
-            nextState(c, state);
-            System.out.println("After nextstate() "+state);
-        }
-		System.out.println("-> State at the end: "+state);
+			char c = w.charAt(i);
+			System.out.println("State -> "+ state + " char -> " + c);
+			nextState(c);
+			
+		}
+		System.out.println("-> State at the end "+state);
 		return isCurrentStateAcceptable();
 	}
-	
+	    
 	//move to the next State
-	public void nextState(char c, int state) {
+	public void nextState(char c) {
 		switch(state) {
-			case 0:
-				if(c == 'a')
-					state = 1;
-				else
-					state = -1;
-				break;
-			case 1:
-				if(c == 'b') state = 2;
-				else state = -1;
-				break;
-			default:
-				state = -1;
-				break;
-		}
-		
+		    case 0:
+		    	if(c == 'a') state = 1;
+		    	else state = -1;
+		    	break;
+		    case 1: 
+		    	if(c == 'b') state = 2;
+		    	else state = -1;
+		    	break;
+		    default:
+		    	state = -1;
+		    	break;
+		}        
 	}
+    public static void main(String args[]) {
+        Automate A = new Automate();
+        for (int i = 0; i < args.length; i++) {
+        	String arg = args[i];
+            boolean isReconized = A.isWordReconizable(arg);
+            System.out.println(arg + "" + (isReconized ? " is" : " is not") + " reconized ");
+            System.out.println("\n");
+        }
+    }
 	
-	public static void main(String args[]) {
-		Automate A =  new Automate();
-		for (int i = 0; i < args.length; i++) {
-			String arg = args[i];
-			boolean isReconizable = A.isWordReconizable(arg);
-			if(isReconizable) {
-				System.out.println("The word "+arg+" is reconizable");
-			}else {
-				System.out.println("The word "+arg+" is not reconizable");
-			}
-		}
-	}
+	
 }
